4111659bae782496d999b2763fa810cdd672b953
Artemy <artemij87work@gmail.com>
2022-08-30 12:06:23 +0300

    Updates

---
+++ precache-manifest.b174d76fc526d812ac9917147afec27b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f4884491471d79e718b3127ec8416ff",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "013c0cdea356c9405b79",
+    "url": "/Todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "18e74143fef37480a066",
+    "url": "/Todolist/static/js/2.0f69acc3.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/Todolist/static/js/2.0f69acc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "013c0cdea356c9405b79",
+    "url": "/Todolist/static/js/main.d1a7f901.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
